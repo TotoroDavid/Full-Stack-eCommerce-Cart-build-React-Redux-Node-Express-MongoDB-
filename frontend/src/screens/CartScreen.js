7f2f8ed2f7b24d6f94e84004f5fb8685b773ab6f
TotoroDavid <davidtoro65@hotmail.com>
2021-06-29 10:15:32 -0500

    setting the toggle navbar

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CartScreen.css'
+
+const CartScreen = () => {
+    return (
+        <divc className="">
+            cartScreen
+        </divc>
+    )
+}
+
+export default CartScreen
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ProductScreen.css'
+
+const ProductScreen = () => {
+    return (
+        <div className="">
+            ProductScreen
+        </div>
+    )
+}
+
+export default ProductScreen
